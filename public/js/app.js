b1636643c68d0dc09bfcef36cf915b55214a418d
James <rotichjames8@gmail.com>
2024-08-26 22:07:15 +0300

    node practice

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("Mastery, here I come");
--- src/app.js
+++ src/app.js
@@ -1,22 +1,16 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello Express!");
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("/help", (req, res) => {
-  res.send("help page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>This is the about page</h1>");
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send({ location: "karatina", temperature: 21 });
 });
+
 app.listen(3000, () => {
   console.log("Server is running in port 3000");
 });
